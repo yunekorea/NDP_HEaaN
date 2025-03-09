9efaf35564e97b0992065b3a29e51c32e57ed8d6
hunsy9 <juniper0917@gmail.com>
2025-03-09 12:34:26 +0900

    Remove DPDK submodule

--- spdk/dpdk/lib/mbuf/mbuf_log.h
+++
@@ -1,6 +0,0 @@
-/* SPDX-License-Identifier: BSD-3-Clause */
-
-extern int mbuf_logtype;
-#define RTE_LOGTYPE_MBUF	mbuf_logtype
-#define MBUF_LOG(level, ...) \
-	RTE_LOG_LINE(level, MBUF, "" __VA_ARGS__)

62af099a188abb6310d218a1e21d4b35fa9c7cf9
yunekorea <kooopooo@naver.com>
2025-08-28 14:12:50 +0900

    Fixed data direction's opcode error.(Caused by the dev.)

--- spdk/include/spdk/nvmf_transport.h
+++ spdk/include/spdk/nvmf_transport.h
@@ -671,8 +671,9 @@ spdk_nvmf_req_get_xfer(struct spdk_nvmf_request *req) {
         return SPDK_NVME_DATA_CONTROLLER_TO_HOST;
     }
 
-	if (cmd->opc == 0xE1) { // HEaaN Cipadd command
-        return SPDK_NVME_DATA_HOST_TO_CONTROLLER;
+	if (cmd->opc == 0xE0) { // HEaaN Cipadd command
+        //return SPDK_NVME_DATA_HOST_TO_CONTROLLER;
+        return SPDK_NVME_DATA_CONTROLLER_TO_HOST;
 	}
 
 	/* Figure out data transfer direction */
